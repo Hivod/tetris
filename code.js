b63e1e572909b6473855ad66785bdf0761f22a07
Hivod <hivod00@gmail.com>
2018-02-15 20:40:43 +0100

    rotation

--- code.js
+++ code.js
@@ -70,10 +70,38 @@ function drawFrame() {
 }
 
 /* controls (left + right) */
+function rotate() {
+  var arr = new Array;
+  for (var i = 0; i < player.tetro.length; i++) {
+    arr.push([]);
+  }
+  for (var y = 0; y < player.tetro.length; y++) {
+    for (var x = 0; x < player.tetro[y].length; x++) {
+      arr[y].push(player.tetro[x][y]);
+    }
+  }
+  arr.reverse();
+  return arr;
+}
+
+function move(direction) {
+  player.x += direction;
+  if (checkCollision(arena, player)) {
+    player.x -= direction;
+  }
+}
+
 document.body.addEventListener("keydown", function (key) {
-  if (key.keyCode == 37) player.x--;
-  if (key.keyCode == 39) player.x++;
-  // if(key.keyCode == 38) rotate();
+  if (key.keyCode == 37) move(-1);
+  if (key.keyCode == 39) move(1);
+  if(key.keyCode == 38) {
+    player.tetro = rotate(player.tetro);
+    if (checkCollision(arena, player)) {
+      for(var i = 0; i < 3; i++) {
+        player.tetro = rotate(player.tetro);
+      }
+    }
+  }
   if (key.keyCode == 40) moveDown();
 });
 
@@ -93,7 +121,7 @@ function placeTetro(arena, player) {
 function checkCollision(arena, player) {
   for (var y = 0; y < player.tetro.length; y++) {
     for (var x = 0; x < player.tetro[y].length; x++) {
-      if (player.tetro[y][x] == 1 && arena[player.y + y][player.x + x] == 1) {
+      if (player.tetro[y][x] != 0 && (arena[player.y + y] && arena[y + player.y][x + player.x] != 0)) {
         return true;
       }
     }
@@ -110,8 +138,10 @@ function moveDown() {
     console.table(arena);
     player.y = 0;
     player.x = 4;
+    player.tetro = tetrominos[Math.floor(Math.random() * 7)];
   }
 }
+
 
 var dropCounter = 0;
 var dropInterval = 1000;
